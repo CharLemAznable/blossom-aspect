ec5fee485eb62ccce299e45105298e47838e91c4
CharLemAznable <545541819@qq.com>
2023-01-03 18:44:29 +0800

    Some refactor.

--- src/test/java/blossom/test/aspect/spring/SpringAspectBlossomTest.java
+++ src/test/java/blossom/test/aspect/spring/SpringAspectBlossomTest.java
@@ -6,17 +6,12 @@ import blossom.test.aspect.TestAspectNoneController;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = SpringAspectBlossomConfig.class)
-@WebAppConfiguration
+@SpringJUnitWebConfig(SpringAspectBlossomConfig.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class SpringAspectBlossomTest extends TestAspectBlossom {
 
